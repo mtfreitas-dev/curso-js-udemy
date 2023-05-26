429dd708a44736608d375d41ae3f1e8b84b06914
Matheus Freitas <122810512+mtfreitas-dev@users.noreply.github.com>
2023-05-26 09:56:49 -0300

    Update

---
+++ Aulas/Seção 7- Programação Orientada a Objetos/aula107/index.js
@@ -0,0 +1,14 @@
+/*class ValidaFormulario{
+    constructor(){
+        this.formulario = document.querySelector('.formulario')
+       this.eventos();
+    }
+
+    eventos(){
+        this.formulario.addEventListener('submit', e=> {
+            window('Enviado')
+        });
+    }
+}
+
+const valida = ValidaFormulario();*/
