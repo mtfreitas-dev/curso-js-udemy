bc6d173af49140a3822ca0e192484f3bc97bb5e8
Matheus Freitas <122810512+mtfreitas-dev@users.noreply.github.com>
2023-07-20 08:30:07 -0300

    Update secao 10

--- Aulas/Seção 10 - Node, Npm, Express e MongoDB(JS)/aula133/server.js
+++ Aulas/Seção 10 - Node, Npm, Express e MongoDB(JS)/aula133/server.js
@@ -1,34 +1,9 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes')
 
-app.use(
-    express.urlencoded(
-        {
-            extended: true
-        }
-    )
-)
-
-app.get('/', (req, res) => {
-    res.send(`
-    <form action="/" method="POST">
-    Nome: <input type="text" name="nome">
-    <button>Enviar</button>
-    <form>
-    `);      
-});
-
-app.get('/testes/:idUsuario?/:parametro?', (req, res) => {
-    console.log(req.query);
-    console.log(req.query);
-    res.send(req.params);
-});
-
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.send(`O que você me enviou foi: ${req.body.nome}`);
-})
-
+app.use(express.urlencoded({extended: true}));
+app.use(routes);
 
 app.listen(3000, () => {
     console.log('Acessar http://localhost:3000')
